0eeac38bae256401a5f49bd9da516e41d48c599f
root <root@DESKTOP-U0DVLUA.localdomain>
2020-04-04 11:12:21 +0100

    vintage index and collapsible

---
+++ assets/js/collapsible.js
@@ -0,0 +1,34 @@
+(function collapsible() {
+
+    var collapsibles = document.getElementsByClassName("collapsible");
+    
+    
+    for (var i = 0; i < collapsibles.length; i++) {
+        var collapsible = collapsibles[i];
+
+        var container = document.createElement("div");
+        container.innerHTML = collapsible.innerHTML;
+        collapsible.innerHTML = "";
+        collapsible.appendChild(container);
+
+        var expander = document.createElement("div");
+        expander.className = "expander";
+        expander.innerText = "˅";
+        collapsible.appendChild(expander);
+
+        container.innerHTML = collapsible.innerHTML
+
+        expander.onclick = function() {
+            if (collapsible.className.indexOf("expanded") > -1) {
+                expander.innerText = "˅";
+                collapsible.style.height = "180px";
+                collapsible.className = collapsible.className.replace(" expanded","");
+            } else {
+                expander.innerText = "˄";
+                collapsible.style.height = (container.clientHeight + 36) + "px";
+                collapsible.className += " expanded";
+            }
+        }
+        
+    }
+})();
