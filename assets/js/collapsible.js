cd9d8f0d08a83e76a54d4e6fed7dd22670119293
root <root@DESKTOP-U0DVLUA.localdomain>
2020-04-08 21:27:16 +0100

    add collapsible to pos

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -11,6 +11,13 @@
         collapsible.innerHTML = "";
         collapsible.appendChild(container);
 
+        //If container is small then remove collapsible style entirely
+        //and don't add the expander button
+        if (container.clientHeight < 240) {
+            collapsible.className = collapsible.className.replace(" collapsible","");
+            return;
+        }
+        
         var expander = document.createElement("div");
         expander.className = "expander";
         expander.innerText = "˅";
